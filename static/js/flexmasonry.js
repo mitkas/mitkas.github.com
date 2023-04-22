8758fc3bd203306c0372126d8a7f55b567ef1532
Mitka <dimitriy@gmail.com>
2023-04-22 23:02:13 +0200

    4 column

--- static/js/flexmasonry.js
+++ static/js/flexmasonry.js
@@ -72,6 +72,8 @@ var FlexMasonry = (function (e) {
       breakpointCols: {
         "min-width: 1500px": 6,
         "min-width: 1200px": 5,
+        // "min-width: 1500px": 4,
+        // "min-width: 1200px": 4,
         "min-width: 992px": 4,
         "min-width: 768px": 3,
         "min-width: 576px": 2,
